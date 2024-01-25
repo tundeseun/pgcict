491e200f3f35b5b8e251ab8290987009c2905a03
sade <alimfolasade@gmail.com>
2024-01-25 11:54:22 +0100

    modified

--- connect.php
+++ connect.php
@@ -1,13 +1,4 @@
 <?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = 'BAbalola2018';
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
-$dbname = 'pgcgpa';
-mysqli_select_db($dbname);
 
-//echo (date('d-n-Y') + 1);
-//echo date('tomorrow');
-//echo date("Y-m-d",strtotime("tomorrow"));
 ?> 
 
