7365344447f2413283c02a558bad0f6adb18911c
aibeekhay <ibukunadeyemi4@gmail.com>
2024-01-25 10:54:31 +0100

    services created

---
+++ services.php
@@ -0,0 +1,11 @@
+<h3>
+    Our services include the following:
+</h3>
+
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Vestibulum porttitor maximus neque ac faucibus. Proin tincidunt malesuada congue. 
+Pellentesque et porta dui. Nunc pharetra dolor ac porta venenatis. Proin a turpis nulla. 
+Curabitur vel metus nec sem efficitur luctus a et risus. 
+Nam lacinia iaculis varius. Mauris sed lorem aliquet mauris laoreet vulputate
+</p>
